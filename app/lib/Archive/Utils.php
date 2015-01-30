16641cb32dbc8ef86f0df9c34324ab661130412e
Oscar Berry <oscar@madokami.com>
2015-01-30 20:32:25 +0000

    Make reader extension check case-insensitive

--- app/lib/Archive/Utils.php
+++ app/lib/Archive/Utils.php
@@ -9,6 +9,7 @@ class Utils {
 
         foreach($files as $file) {
             $ext = pathinfo($file, PATHINFO_EXTENSION);
+            $ext = strtolower($ext);
 
             if(in_array($ext, array('jpg', 'jpeg', 'png'))) {
                 $result[] = $file;
