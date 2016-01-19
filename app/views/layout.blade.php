5e45ce8ae2f381aa539eb5392107ebf1faa7051b
queue <queue@hakase.org>
2016-01-19 01:35:21 +0000

    Improve search engine cloaking
    Allow googlebot and bingbot to crawl and pick up the
    meta robots tag, so we won't show up even if other sites link
    to us. It seems that ia_archiver's Heritrix bot doesn't honor
    noindex, so that's fine.
    
    Bots of other engines don't have good data on whether they
    honor noindex, so continue blocking at the robots.txt level.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="googlebot" content="noindex">
+    <meta name="robots" content="noindex,nofollow">
 
     <meta name="viewport" content="width=device-width" />
     <meta name="mobile-web-app-capable" content="yes">
