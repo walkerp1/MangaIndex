1639316194fdd77a0bc1a35b775c05554dfe6e88
queue <queue@hakase.org>
2016-05-10 02:39:15 +0000

    RSS: add dirs to feeds where possible

--- app/views/index-rss.blade.php
+++ app/views/index-rss.blade.php
@@ -6,14 +6,16 @@
     <description>{{htmlspecialchars( $path->getUrl() , ENT_QUOTES)}} - Madokami</description>
     <pubDate>{{htmlspecialchars( (new DateTime(date('Y-m-d H:i:s', $updated)))->format(DateTime::RFC822), ENT_QUOTES)}}</pubDate>
 
-    @foreach($children as $child)@if(!$child->isDir)<item>
+    @foreach($children as $child)<item>
       <title>{{htmlspecialchars( $child->name , ENT_QUOTES)}}</title>
       <link>{{htmlspecialchars(URL::to('/'), ENT_QUOTES)}}{{htmlspecialchars( $child->url , ENT_QUOTES)}}</link>
+      @if(!$child->isDir)
       <enclosure url="{{htmlspecialchars(URL::to('/'), ENT_QUOTES)}}{{htmlspecialchars( $child->url , ENT_QUOTES)}}" type="{{htmlspecialchars($child->mime, ENT_QUOTES)}}" length="{{htmlspecialchars($child->rawSize, ENT_QUOTES)}}" />
+      @endif
       <guid>{{htmlspecialchars(URL::to('/'), ENT_QUOTES)}}{{htmlspecialchars( $child->url , ENT_QUOTES)}}?length={{htmlspecialchars( $child->rawSize , ENT_QUOTES)}}&amp;mtime={{htmlspecialchars( $child->rawTime, ENT_QUOTES)}}</guid>
       <pubDate>{{htmlspecialchars( (new DateTime(date('Y-m-d H:i:s', $child->rawTime)))->format(DateTime::RFC822), ENT_QUOTES)}}</pubDate>
       <description><![CDATA[<a href="{{htmlspecialchars(URL::to('/'), ENT_QUOTES)}}{{htmlspecialchars( $path->getUrl() , ENT_QUOTES)}}">{{htmlspecialchars( $path->getUrl() , ENT_QUOTES)}}</a>/<a href="{{htmlspecialchars(URL::to('/'), ENT_QUOTES)}}{{htmlspecialchars( $child->url , ENT_QUOTES)}}" type="{{htmlspecialchars($child->mime, ENT_QUOTES)}}" length="{{htmlspecialchars($child->rawSize, ENT_QUOTES)}}">{{htmlspecialchars( $child->name , ENT_QUOTES)}}</a>]]></description>
     </item>
-    @endif @endforeach
+    @endforeach
 </channel>
 </rss>
