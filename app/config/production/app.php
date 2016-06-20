3f6b44344344453d8715be08f2e4bf8b09ad4579
queue <queue@hakase.org>
2016-06-20 07:11:20 +0000

    Move to the glorious new domain
    wonder how the old url worked at all

--- app/config/production/app.php
+++ app/config/production/app.php
@@ -4,8 +4,8 @@ return array(
 
     'debug' => false,
     'manga_path' => '/mango',
-    'images_path' => '/srv/http/manga_madokami_com/images',
-    'url' => 'http://madokami.hakase.org',
+    'images_path' => '/srv/http/manga_madokami_al/images',
+    'url' => 'http://manga.madokami.al',
     'require_auth' => true,
 
 );
